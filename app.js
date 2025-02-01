aabf172db344732dc031c5c8de46aac336958bdb
DShort13 <dion.short@outlook.com>
2025-02-01 23:03:59 +0000

    Create centralised error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes/index");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
@@ -14,6 +15,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
