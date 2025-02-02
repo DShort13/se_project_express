80c6f2f9429e6aa895ad2e00169e3335033b0e78
DShort13 <dion.short@outlook.com>
2025-02-02 12:50:30 +0000

    Create validation functions

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,2 +1,31 @@
 const { Joi, celebrate } = require("celebrate");
 const validator = require("validator");
+
+const validateCreateItem = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    imageUrl: Joi.string().required().url(),
+  }),
+});
+
+const validateCreateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().url(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateLogIn = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateItemId = celebrate({
+  params: Joi.object().keys({
+    itemId: Joi.string().required().hex().length(24),
+  }),
+});
