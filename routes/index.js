c406acbfc7a1d2a8a59b72cc770485ea40c41f9b
DShort13 <dion.short@outlook.com>
2025-02-06 16:46:40 +0000

    Update error handling

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItems = require("./clothingItems");
-const { NOT_FOUND } = require("../utils/errors");
 const { createUser, login } = require("../controllers/users");
+const NotFoundError = require("../utils/Errors/NotFoundError");
 
 router.post("/signin", login);
 router.post("/signup", createUser);
 router.use("/users", userRouter);
 router.use("/items", clothingItems);
 
-router.use((req, res) => {
-  res.status(NOT_FOUND).send({
-    message: "Requested resource not found",
-  });
+router.use((req, res, next) => {
+  next(new NotFoundError("Requested resource not found"));
 });
 
 module.exports = router;
